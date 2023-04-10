f9829b8c380c4e438048c3bee38c64e6eb0f593e
alex harding <alexander.harding@gmail.com>
2023-04-10 13:41:43 -0400

    push

---
+++ PolylineOffset.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PolylineOffset2
+{
+    public static List<Vector2> OffsetPolyline(List<Vector2> input, float offset)
+    {
+        int n = input.Count;
+        List<Vector2> offsetVertices = new List<Vector2>(n);
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (i == 0)
+            {
+                Vector2 edge = (input[1] - input[0]).normalized;
+                Vector2 normal = new Vector2(-edge.y, edge.x);
+                offsetVertices.Add(input[0] + normal * offset);
+            }
+            else if (i == n - 1)
+            {
+                Vector2 edge = (input[n - 1] - input[n - 2]).normalized;
+                Vector2 normal = new Vector2(-edge.y, edge.x);
+                offsetVertices.Add(input[n - 1] + normal * offset);
+            }
+            else
+            {
+                Vector2 prev = input[i - 1];
+                Vector2 current = input[i];
+                Vector2 next = input[i + 1];
+
+                Vector2 edge1 = (current - prev).normalized;
+                Vector2 edge2 = (next - current).normalized;
+
+                Vector2 bisector = (edge1 + edge2).normalized;
+                float offsetLength = offset / Mathf.Sin(Mathf.Acos(Vector2.Dot(edge1, edge2)) / 2);
+
+                offsetVertices.Add(current + bisector * offsetLength);
+            }
+        }
+
+        return offsetVertices;
+    }
+}
---
+++ _PolylineOffset.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Very simple polyline offset algorithm
+/// </summary>
+
+class PolylineOffset
+{
+    public static List<Vector2> OffsetPolyline(List<Vector2> input, float offset)
+    {
+        List<Vector2> output = new List<Vector2>();
+
+        for (int i = 0; i < input.Count - 1; i++)
+        {
+            Vector2 A = input[i];
+            Vector2 B = input[i + 1];
+
+            _Vector2 AB = new _Vector2(B.x - A.x, B.y - A.y);
+            _Vector2 normal = AB.Rotate(90).Normalized();
+
+            Vector2 AOffset = A + normal * offset;
+            Vector2 BOffset = B + normal * offset;
+
+            output.Add(AOffset);
+
+            if (i == input.Count - 2)
+            {
+                output.Add(BOffset);
+            }
+        }
+
+        return output;
+    }
+    
+    private struct _Vector2
+    {
+        public float X;
+        public float Y;
+
+        public _Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float Magnitude() => (float)Math.Sqrt(X * X + Y * Y);
+
+        public _Vector2 Normalized()
+        {
+            float mag = Magnitude();
+            return new _Vector2(X / mag, Y / mag);
+        }
+
+        public _Vector2 Rotate(float degrees)
+        {
+            float radians = degrees * (float)Math.PI / 180;
+            float sin = (float)Math.Sin(radians);
+            float cos = (float)Math.Cos(radians);
+
+            return new _Vector2(X * cos - Y * sin, X * sin + Y * cos);
+        }
+
+        public static _Vector2 operator *(_Vector2 a, float scalar) => new _Vector2(a.X * scalar, a.Y * scalar);
+        public static Vector2 operator +(Vector2 a, _Vector2 b) => new Vector2(a.x + b.X, a.y + b.Y);
+    }
+    
+}
+
